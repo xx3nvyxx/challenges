16f363876c235878be7fa818aee62a43e6212023
William Washco <xx3nvyxx@gmail.com>
2016-04-29 15:45:49 -0700

    Small input solved, large input has recursion problems

--- codejam/2008/Practice Problems/C/EggDrop.py
+++ codejam/2008/Practice Problems/C/EggDrop.py
@@ -23,32 +23,28 @@ def EggDrop(F,D,B):
 def maxF(D, B):
     if table.get((D,B), 0) != 0:
         return table[(D,B)]
-    if B > 32:
-        table[(D,B)] = 2**33
-        return 2**33
     if B >= D:
         table[(D,B)] = (2**D)-1
-        return table[(D,B)]
+        return (2**D)-1
     if B == 1:
         table[(D,B)] = D
         return D
-    if D == 1:
-        table[(D,B)] = 1
-        return 1
     table[(D,B)] = maxF(D-1,B) + maxF(D-1, B-1) + 1
     return table[D,B]
 
 def minD(F, B):
-    for d in range(1, 200):
-        if maxF(d,B) >= F:
-            return d
-    return -2
+    if B == 1:
+        return F
+    d = 1
+    while maxF(d,B) < F:
+        d += 1
+    return d 
 
 def minB(F, D):
-    for b in range(1, 33):
-        if maxF(D,b) >= F:
-            return b
-    return -2
+    b = 1
+    while maxF(D,b) < F:
+        b += 1
+    return b
 
 
 if __name__=="__main__":

5c2378e7a40b26b43dc0402d8bea8fd658f80a5b
William Washco <xx3nvyxx@gmail.com>
2017-04-15 03:53:41 +0000

    Solves examples, fails on small input

---
+++ codejam/2017/1A/C/PlaytheDragon.py
@@ -0,0 +1,45 @@
+#!/user/bin/env python
+"""Google Code Jam 2017: Problem C. Play the Dragon"""
+
+from __future__ import print_function
+from builtins import input
+
+def main():
+    T = int(input())
+    if T < 1 or T > 100:
+        raise RuntimeError("T is not within the valid range")
+    for i in range(T):
+        Hd,Ad,Hk,Ak,B,D = map(int, input().split())
+        r = PlaytheDragon(Hd,Ad,Hk,Ak,B,D)
+        print("Case #" + str(i + 1) + ": " + r)
+
+def PlaytheDragon(Hd,Ad,Hk,Ak,B,D):
+    H = Hd
+    queue = []
+    state = [0,Hd,Ad,Hk,Ak]
+    queue.append(state)
+    seen = set()
+    while len(queue) > 0:
+        turn,Hd,Ad,Hk,Ak = queue.pop(0)
+        tag = str(Hd) + " " + str(Ad) + " " + str(Hk) + " " + str(Ak)
+        if tag in seen:
+            continue
+        else:
+            seen.add(tag)
+        turn += 1
+        if Ad >= Hk:
+            return str(turn)
+        if Ak < Hd:
+            attack = (turn,Hd-Ak,Ad,Hk-Ad,Ak)
+            queue.append(attack)
+            buff   = (turn,Hd-Ak,Ad+B,Hk,Ak)
+            queue.append(buff)
+            debuff = (turn,Hd-Ak,Ad,Hk,max(Ak-D,0))
+            queue.append(debuff)
+        if Ak < H:
+            heal = (turn,H-Ak,Ad,Hk,Ak)
+            queue.append(heal)
+    return "Impossible"
+
+if __name__=="__main__":
+    main()

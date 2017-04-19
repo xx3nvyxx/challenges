6c3b17180d22d4364fe43c4c8e4150a5dfb714ca
William Washco <xx3nvyxx@gmail.com>
2017-04-19 23:41:29 +0000

    Solves small and large inputs

--- codejam/2008/1B/A/CropTriangles.py
+++ codejam/2008/1B/A/CropTriangles.py
@@ -14,25 +14,37 @@ def main():
         print("Case #" + str(i + 1) + ": " + r)
 
 def CropTriangles(n,A,B,C,D,X,Y,M):
-    trees = set()
-    trees.add((X,Y))
-    r = set()
+    trees = dict()
+    trees[(X%3,Y%3)] = 1
+    r = 0
     checked = set()
     for i in range(1,n):
         X = (A * X + B) % M
         Y = (C * Y + D) % M
-        trees.add((X,Y))
-    for a in trees:
-        checked.add(a)
-        for b in trees-checked:
-            if a == b:
-                continue
-            for c in trees-checked:
-                if a == c or b == c:
+        trees[(X%3,Y%3)] = trees.get((X%3,Y%3),0)+1
+    for a in trees.keys():
+        for b in trees.keys():
+            for c in trees.keys():
+                if frozenset({a,b,c}) in checked:
                     continue
+                else:
+                    checked.add(frozenset({a,b,c}))
                 if (a[0]+b[0]+c[0]) % 3 == 0 and (a[1]+b[1]+c[1]) % 3 == 0:
-                    r.add(frozenset({a,b,c}))
-    return str(len(r))
+                    if a == b and b == c:
+                        t = trees[a]
+                        r += (t*(t-1)*(t-2))/6
+                    elif a == b:
+                        t = trees[a]
+                        r += trees[c] * (t*(t-1))/2
+                    elif a == c:
+                        t = trees[a]
+                        r += trees[b] * (t*(t-1))/2
+                    elif b == c:
+                        t = trees[b]
+                        r += trees[a] * (t*(t-1))/2
+                    else:
+                        r += trees[a]*trees[b]*trees[c]
+    return str(r)
 
 if __name__=="__main__":
     main()
